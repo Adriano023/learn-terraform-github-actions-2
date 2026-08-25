204a2e41241cc414b045f2e1ef2e89a11f21a065
Adriano <la_tua_email_di_github@example.com>
2026-08-25 21:58:59 +0200

    Aggiornamento regione eu-north-1 e nuovo AMI ID

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME" # <-- INSERISCI QUI SOLO IL NOME DELLA TUA ORGANIZZAZIONE
+    organization = "test_02332" # <-- INSERISCI QUI SOLO IL NOME DELLA TUA ORGANIZZAZIONE
 
     workspaces {
       name = "gh-actions-demo" 
