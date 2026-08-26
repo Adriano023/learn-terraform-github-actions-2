f56b9ce05b33cf6bf51882666e7a9e8dfd491e8f
Adriano <la_tua_email_di_github@example.com>
2026-08-26 12:01:13 +0200

    Integrazione Docker: installazione motore e avvio microservizio containerizzato

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "test_02332" # <-- INSERISCI QUI SOLO IL NOME DELLA TUA ORGANIZZAZIONE
+    organization = "test_02332" # <-- INSERISCI QUI LA TUA ORGANIZZAZIONE HCP
 
     workspaces {
       name = "gh-actions-demo" 
@@ -20,20 +20,29 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-north-1" # <-- REGIONE DI STOCCOLMA
+  region = "eu-north-1"
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-0aba19e56f3eaec05" # <-- AMI DI STOCCOLMA
+  ami           = "ami-0aba19e56f3eaec05"
   instance_type = "t3.micro" 
 
+  # Il blocco user_data ora automatizza l'installazione di Docker
   user_data = <<-EOF
               #!/bin/bash
+              # 1. Aggiorna i repository di sistema
               apt-get update
-              apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
-              systemctl restart apache2
+              
+              # 2. Installa il motore Docker
+              apt-get install -y docker.io
+              
+              # 3. Abilita e avvia il demone Docker
+              systemctl enable docker
+              systemctl start docker
+              
+              # 4. Scarica e avvia il container del microservizio
+              # Mappiamo la porta 8080 della macchina virtuale sulla porta 80 del container
+              docker run -d -p 8080:80 --name microservizio-web nginx:latest
               EOF
 }
 
